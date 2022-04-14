5c604f336ec8b53955d94e93a68de6a1a7c3de27
ARAVA KRISHNAVENI <aravakrishnaveni@ARAVAs-MacBook-Air.local>
2022-04-14 14:16:11 +0530

    added a router controller and set up views and layouts

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+app.use(express.static('assets'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.use('/',require('./routes/index'));
+
 app.listen(port,function(err){
     if(err){
         console.log("Error",err);
